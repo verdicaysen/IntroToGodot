f968bc48e166ebe5afb2267d9e0db90352e7c5f3
verdicaysen <verdicaysen@gmail.com>
2023-09-19 22:53:06 -0500

    Fun with functions.

--- IntroToScripting.gd
+++ IntroToScripting.gd
@@ -1,13 +1,23 @@
 extends Node2D
 
-
+var gameOver: bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	var result = Add(10, 25)
+	print(result)
 	pass
 
+func Add(a,b):
+	var sum = a + b
+	return sum
+	pass
+
+func WelcomeMessage():
+	
+	print("Welcome to Intro to Scripting!")
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-	
